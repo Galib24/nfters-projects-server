c058c0d3944d607cbb3b70af242f15f7fc10cdfc
SQLAdmin <abuyeahia24@gmail.com>
2023-06-27 23:35:04 +0600

    set up all data by mongodb

---
+++ index.js
@@ -0,0 +1,63 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+// mongodb part 
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z8yqdyj.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    // collection part by mongodb
+    const dataCollection = client.db('nftersDb').collection('Collections');
+
+    app.get('/collections', async (req, res)=>{
+        const result = await dataCollection.find().toArray();
+        res.send(result);
+    })
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res)=>{
+    res.send('nfts is ok')
+});
+
+app.listen(port, ()=>{
+    console.log(`nft is running on port ${port}`);
+})
